bb86c45e97e4c50ecf59e7423762e5a3e1f458dc
HonzikCZI <jan.sara.cze@gmail.com>
2025-01-22 15:58:33 +0100

    Enable fullscreen mode and add time display to the Clicker Game

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import pygame
 pygame.init()
 
 # Nastavení obrazovky
-
 width = 1280
 height = 720
-screen = pygame.display.set_mode((width, height))
+fullscreen = True
+screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
 pygame.display.set_caption("CLIKER GAME")
 
-# Nestavení hry
+# Nastavení hry
 fps = 120
 coin = 0
-cena_klikání = 50
-klikání = 1
+cena_klikani = 50
+klikani = 1
 #cena_autoclicku = 500 - možná bude ve hře
 
 # Barvy
@@ -26,15 +26,15 @@ big_font = pygame.font.Font("fonts/Emulogic.ttf", 50)
 midle_font = pygame.font.Font("fonts/Emulogic.ttf", 30)
 
 # Obrazky
-pozadí = pygame.image.load("img/obrazek.png")
-pozadí = pygame.transform.scale(pozadí, (width, height))
-pozadí_rect = pozadí.get_rect()
-pozadí_rect.topleft = (0, 0)
+pozadi = pygame.image.load("img/obrazek.png")
+pozadi = pygame.transform.scale(pozadi, (width, height))
+pozadi_rect = pozadi.get_rect()
+pozadi_rect.topleft = (0, 0)
 
-myš = pygame.image.load("img/mouse.png")
-myš = pygame.transform.scale(myš, (100, 100))
-myš_rect = myš.get_rect()  
-myš_rect.center = (width//2, height//2)
+mys = pygame.image.load("img/mouse.png")
+mys = pygame.transform.scale(mys, (100, 100))
+mys_rect = mys.get_rect()  
+mys_rect.center = (width//2, height//2)
 
 # Texty
 coin_text = midle_font.render(f"coins: {coin}", True, black)
@@ -44,23 +44,33 @@ coin_text_rect.topleft = (10, 10)
 # Frame rate control
 clock = pygame.time.Clock()
 
-# Hlavní cyklus
-lets_continue = True
-
-while lets_continue:
+# Hlavní smyčka hry
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            lets_continue = False
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_F10:
+                running = False
+            elif event.key == pygame.K_F5:
+                fullscreen = not fullscreen
+                if fullscreen:
+                    screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
+                else:
+                    screen = pygame.display.set_mode((width, height))
 
     # obrázky
-    screen.blit(pozadí, pozadí_rect)
-    screen.blit(myš, myš_rect)
+    screen.blit(pozadi, pozadi_rect)
+    screen.blit(mys, mys_rect)
 
     # Update text
     coin_text = midle_font.render(f"coins: {coin}", True, black)
+    timeText= midle_font.render(f"Time: {pygame.time.get_ticks()//1000}", True, black)
     
     # texty
     screen.blit(coin_text, coin_text_rect)
+    screen.blit(timeText, (10, 50))
     
     # update obrazovky
     pygame.display.update()
